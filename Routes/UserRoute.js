def2534aec440ebc4ee985855fb9e53049501960
Elghorez <ellamjed@gmail.com>
2021-08-01 19:07:58 -0700

    final commit

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,16 +1,55 @@
 const express = require('express')
 const User=require('../models/User')
-router = express.Router();
-//PostNewUser
-router.post('/Users', async (req,res)=>{
-    try {
-            const newUser = new User(req.body);
-            const  response = await newUser.save();
-            res.send({res:response, msg:'User is saved :)'})
-    } catch (error) {
-        res.status(400).send("cannot save User :(")
-        console.log(error)
-    }
+    router = express.Router();
+    //PostNewUser
+    router.route('/addUser').post((req, res, next) => {
+        User.create(req.body, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            console.log(data)
+            res.json(data)
+        }
+        })
+    });
 
-})
-module.exports=router;
+    // READ Users
+    router.route('/allusers').get((req, res) => {
+        User.find((error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+        })
+    });
+    // Update Users
+    router.route('/update-user/:id').put((req, res, next) => {
+        User.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+        }, (error, data) => {
+        if (error) {
+            return next(error);
+            console.log(error)
+        } else {
+            res.json(data)
+            console.log('User updated successfully !')
+        }
+        })
+    });
+
+    // Delete User
+    router.route('/delete-user/:id').delete((req, res, next) => {
+        User.findByIdAndDelete(req.params.id, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.status(200).json({
+            msg: data
+            })
+        }
+        })
+    })
+    
+
+    module.exports=router;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express') 
 require('dotenv').config({path:'./config/.env'})
 const connectedb=require('./config/connectdb')
-
 const app = express()
 const PORT=process.env.PORT
 console.log(PORT)
 connectedb()
+
 
 app.use(express.json())
 app.use('/Users', require('./Routes/UserRoute'))
@@ -13,3 +13,4 @@ app.listen(PORT, (err) => {
     (err)?console.error(err):
     console.log(`connected at http://localhost:${PORT}`)
 })
+
